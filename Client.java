c723c2ed8b644f022e9b1abdd3f05e014e3290fd
James McConnel <jcmcconnel@gmail.com>
2024-03-15 11:46:56 -0700

    Initial commit

---
+++ Client.java
@@ -0,0 +1,72 @@
+// A Java program for a Client
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client {
+	// initialize socket and input output streams
+	private Socket socket = null;
+	private DataInputStream input	 = null;
+	private PrintWriter out = null;
+	private Scanner response = null;
+
+	// constructor to put ip address and port
+	public Client(String address, int port)
+	{
+		// establish a connection
+		try {
+			socket = new Socket(address, port);
+			System.out.println("Connected");
+
+			// takes input from terminal
+			input = new DataInputStream(System.in);
+
+			// sends output to the socket
+			out = new PrintWriter(socket.getOutputStream());
+
+         // Receives input from the socket
+         response = new Scanner(socket.getInputStream());
+		}
+		catch (UnknownHostException u) {
+			System.out.println(u);
+			return;
+		}
+		catch (IOException i) {
+			System.out.println(i);
+			return;
+		}
+
+		// string to read message from input
+		String line = "";
+
+		// keep reading until "Over" is input
+		while (!line.equals("Over")) {
+			try {
+				line = input.readLine();
+            out.println(line);
+            out.flush();
+				System.out.println(response.nextLine());
+			}
+			catch (IOException i) {
+				System.out.println(i);
+			}
+		}
+
+		// close the connection
+		try {
+			input.close();
+			out.close();
+         response.close();
+			socket.close();
+		}
+		catch (IOException i) {
+			System.out.println(i);
+		}
+	}
+
+	public static void main(String args[])
+	{
+		Client client = new Client("127.0.0.1", 5000);
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+Initial code example from: https://www.geeksforgeeks.org/socket-programming-in-java/
+
+So, basically I'm thinking of creating an application server.  The Server.java opens a socket.  Nginx can be configured to proxy that localhost port.  
