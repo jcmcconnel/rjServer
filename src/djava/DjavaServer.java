5e0d40e6fb528e0dbec05ba1c1976c2667f0a2ea
James McConnel <jcmcconnel@gmail.com>
2024-03-17 11:10:16 -0700

    stashing

--- src/djava/DjavaServer.java
+++ src/djava/DjavaServer.java
@@ -10,51 +10,52 @@ import java.util.List;
 
 public class DjavaServer
 {
-	//initialize socket and input stream
-	private Socket socket;
-	private ServerSocket server;
-	private Scanner in;
-	private PrintWriter out;
+   private Socket socket;
+   private ServerSocket server;
+   private Scanner in;
+   private PrintWriter out;
 
-	public DjavaServer()
-	{
+   private List<djava.Responder> responders
+   
+   public DjavaServer()
+   {
       socket = null;
       server = null;
       in = null;
       out = null;
-	}
-
-	public void start(int port)
-	{
-		try {
-		// starts server and waits for a connection
-			server = new ServerSocket(port);
-			System.out.println("Server started");
+   }
+   
+   public void start(int port)
+   {
+   	try {
+   	// starts server and waits for a connection
+   		server = new ServerSocket(port);
+   		System.out.println("Server started");
          while(true) clientSession();
-		}
-		catch(IOException i)
-		{
-			System.out.println(i);
-		}
-	}
-
+   	}
+   	catch(IOException i)
+   	{
+   		System.out.println(i);
+   	}
+   }
+   
    private void clientSession() throws IOException 
    {
       HashMap<String, String> request = new HashMap<String, String>();
       HashMap<String, String> response = new HashMap<String, String>();
-
+   
       System.out.println("Waiting for a client ...");
-
+   
       socket = server.accept();
       System.out.println("Client accepted");
-
+   
       // takes input from the client socket
       in = new Scanner(socket.getInputStream());
-
+   
       out = new PrintWriter(socket.getOutputStream());
-
+   
       String line = "";
-
+   
       if(in.hasNextLine()) {
          line = in.nextLine();
          if(line.equals("echoMode")){
@@ -67,7 +68,7 @@ public class DjavaServer
                   out.println("This is what I received: "+line);
                   out.flush();
                }
-
+   
             }
          } else {
             System.out.println("Accepted client in http mode");
@@ -78,12 +79,12 @@ public class DjavaServer
          }
       }
       System.out.println("Closing connection");
-
+   
       // close connection
       socket.close();
       in.close();
    }
-
+   
    private void readInRequest(Scanner in, HashMap<String, String> request){
       Boolean endOfInput = false;
       String line = request.get("request-line");
@@ -102,7 +103,7 @@ public class DjavaServer
          }
       }
    }
-
+   
    private HashMap<String, String> getResponse(HashMap<String, String> request){
       HashMap<String, String> response = new HashMap<String, String>();
       List responders = Arrays.asList(
@@ -110,7 +111,7 @@ public class DjavaServer
          "/test",
          "/test1"
       );
-
+   
       String responseBody = "";
       if(
          request.get("request-line").startsWith("GET") && 
@@ -119,7 +120,7 @@ public class DjavaServer
          response.put("status-line","HTTP/1.0 200 OK");
          response.put("Content-Type", "text/html; charset=utf-8");
          response.put("Content-Length", "0");
-
+   
          responseBody = getResponseFor((String)responders.get(responders.indexOf(request.get("request-line").split(" ")[1])));
          response.put("Content-Length", String.valueOf(responseBody.length()));
          response.put("body", responseBody);
@@ -127,14 +128,14 @@ public class DjavaServer
       } else {
          response.put("status-line","HTTP/1.0 404 NOT FOUND");
          response.put("Content-Type", "text/html; charset=utf-8");
-
+   
          responseBody = getResponseFor("error");
          response.put("Content-Length", String.valueOf(responseBody.length()));
          response.put("body", responseBody);
       }
       return response;
    }
-
+   
    private void writeResponse(PrintWriter out, HashMap<String, String> response) {
       out.println(response.get("status-line"));
       Iterator i = response.keySet().iterator();
@@ -147,7 +148,7 @@ public class DjavaServer
       out.println(response.get("body"));
       out.flush();
    }
-
+   
    private String getResponseFor(String target){
       if(target.equals("/")){
          return "<!DOCTYPE html>\n"+
@@ -187,7 +188,7 @@ public class DjavaServer
             "</html>\n";
       }
    }
-
-
+   
+   
 }
 
